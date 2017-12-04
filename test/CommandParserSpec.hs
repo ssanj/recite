5b2d377a8856dd91fe63712df62f526a25c7886a
sanjiv sahayam <sanjsmailbox@gmail.com>
2017-12-04 11:17:57 +1100

    add test for invalid match value parser

--- test/CommandParserSpec.hs
+++ test/CommandParserSpec.hs
@@ -1,9 +1,10 @@
 module CommandParserSpec where
 
-import Data.Either (isRight)
+import Data.Either (isLeft, isRight)
 -- import Data.List (intercalate)
 -- import Data.Foldable (concatMap)
 import Test.Tasty
+import Test.Tasty.QuickCheck
 import Test.Tasty.HUnit
 -- import qualified Types as T
 import qualified CommandParser as P
@@ -11,12 +12,19 @@ import qualified CommandParser as P
 import Text.Parsec
 -- import CommonParser (P, tagP, tagsP)
 
-matchValuePTest :: TestTree
-matchValuePTest = let chars = "*?^"
-                      parseResults = fmap (parse P.matchValueP "" . (: [])) chars
-                      results = mapM_ (\r -> assertBool ("expected Right got: " ++ show r) (isRight r)) parseResults
-                  in testCase ("matchValue parser should match one of " ++ chars) results
+matchValueChars :: String
+matchValueChars = "*?^"
 
+matchValuePTest :: TestTree
+matchValuePTest = let parseResults = fmap (parse P.matchValueP "" . (: [])) matchValueChars
+                      results = mapM_ (\r -> assertBool ("expected Right got: " ++ show r) (isRight r)) parseResults
+                  in testCase ("matchValue parser should match one of " ++ matchValueChars) results
+
+matchValuePInvalidProperty :: Property
+matchValuePInvalidProperty = property (\c -> collect c $ (c `notElem` matchValueChars) ==> isLeft $ parse P.matchValueP "" [c])
+
+matchValuePInvalidTest :: TestTree
+matchValuePInvalidTest = testProperty "matchValue parser does not match other chars"  matchValuePInvalidProperty
 
 test_commandParser :: TestTree
-test_commandParser = testGroup "CommandParser" [matchValuePTest]
+test_commandParser = testGroup "CommandParser" [matchValuePTest, matchValuePInvalidTest]
