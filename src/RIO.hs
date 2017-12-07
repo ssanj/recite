fd1f9a9bf53a4e155080c881abb7bea45b26b37e
sanjiv sahayam <sanjsmailbox@gmail.com>
2017-12-08 00:52:43 +1100

    refactor print statements

--- src/RIO.hs
+++ src/RIO.hs
@@ -39,11 +39,11 @@ recite :: String -> IO ()
 recite configFileName =
   do contentsOrError <- readConfig configFileName
      case contentsOrError of
-            Left  (FileReadError msg) -> E.die $ "Failed to load config: " ++ msg
-            Right contents -> (loopHome . CP.parseEntries.lines) contents
+            Left  (FileReadError msg) -> exitWithConfigError msg
+            Right contents            -> (loopHome . CP.parseEntries.lines) contents
 
 loopHome :: [T.Entry] -> IO ()
-loopHome entries = displayInstructions >> loopInstructions entries
+loopHome entries = printInstructions >> loopInstructions entries
 
 ioExToString :: CE.IOException -> String
 ioExToString = CE.displayException
@@ -55,26 +55,22 @@ readConfig :: String -> IO (Either XError String)
 readConfig configFile = fileContentOrError `M.liftM` CE.try (readFile configFile)
 
 loopInstructions :: [T.Entry] -> IO ()
-loopInstructions entries = do line <- getLine
+loopInstructions entries = do line            <- getLine
                               let instruction = parseInstruction line
                               performInstruction instruction entries
 
-displayInstructions :: IO ()
-displayInstructions = putStrLn "Enter a query or press :q to quit"
-
 parseInstruction :: String -> Instruction
-parseInstruction ":q" = QuitQuery
+parseInstruction ":q"    = QuitQuery
 parseInstruction command = either InvalidQuery ValidQuery (P.parse queryP "" command)
 
 performInstruction :: Instruction -> [T.Entry] -> IO ()
-performInstruction QuitQuery _ = E.exitSuccess
-performInstruction (InvalidQuery _) entries =
-  putStrLn ("your command was invalid. Format: " ++ commandFormatString) >> loopInstructions entries
-performInstruction (ValidQuery q) entries =
-    do _ <- putStrLn ("searching for " ++ L.intercalate "," (T.queryTags q))
+performInstruction QuitQuery _              = exit
+performInstruction (InvalidQuery _) entries = printQueryFormatAndLoopInstructions entries
+performInstruction (ValidQuery q) entries   =
+    do _           <- printSearchString q
        let results = filter (S.matches q) entries
-       _ <- putStrLn (printMatchResults results)
-       loopAction results
+       printMatchResults results >> loopAction results
+
 
 parseActionCommand :: [T.Entry] -> String -> ActionCommand
 parseActionCommand _ ":q"        = QuitSearch
@@ -82,8 +78,8 @@ parseActionCommand _ ":h"        = Home
 parseActionCommand results other =
   let actionResult = P.parse actionP "" other
   in case actionResult of
-       Left e           -> NotAction e
-       Right (index, r) ->
+       Left e             -> NotAction e
+       Right (index, r)   ->
          let isValidIndex = index >= 1 && length results >= index
          in if isValidIndex then ValidAction index r
             else InvalidIndex index (oneBasedIndex results) r
@@ -93,25 +89,53 @@ oneBasedIndex [] = 0
 oneBasedIndex xs = length xs + 1
 
 loopAction :: [T.Entry] -> IO ()
-loopAction []      = putStrLn "No matches found" >> E.exitSuccess
+loopAction []      = printNoMatchesAndExit
 loopAction results =
-  do _ <- askAction
-     actionInput <- getLine
+  do _                 <- printActionOptions
+     actionInput       <- getLine
      let actionCommand = parseActionCommand results actionInput
      case actionCommand of
-       QuitSearch -> E.exitSuccess
-       Home  -> loopHome results
-       (NotAction _) -> putStrLn "Invalid action " >> loopAction results
-       InvalidIndex _ options _ ->
-         putStrLn ("Invalid Index. Please choose a number between 1 and " ++ show options) >>
+       QuitSearch               -> exit
+       Home                     -> loopHome results
+       (NotAction _)            -> printActionErrorAndLoopAction results
+       InvalidIndex _ options _ -> printInvalidIndexAndLoopAction options results
+       ValidAction index action -> printActionAndExit action results index
+
+printInstructions :: IO ()
+printInstructions = putStrLn "Enter a query or press :q to quit"
+
+
+printActionOptions :: IO ()
+printActionOptions = putStrLn "Please select a number and an action to perform. Actions can be one of (c) Copy to clipboard (b) Open in browser.\nSelect :h to go to the home screen or :q to quit"
+
+printQueryFormatAndLoopInstructions :: [T.Entry] -> IO ()
+printQueryFormatAndLoopInstructions entries = putStrLn ("your command was invalid. Format: " ++ commandFormatString) >> loopInstructions entries
+
+printSearchString :: T.Query -> IO ()
+printSearchString q = putStrLn $ "searching for " ++ L.intercalate "," (T.queryTags q)
+
+printNoMatchesAndExit :: IO ()
+printNoMatchesAndExit = putStrLn "No matches found" >> exit
+
+printActionErrorAndLoopAction :: [T.Entry] -> IO ()
+printActionErrorAndLoopAction results = putStrLn "Invalid action " >> loopAction results
+
+printInvalidIndexAndLoopAction :: Int -> [T.Entry] -> IO ()
+printInvalidIndexAndLoopAction options results =
+  putStrLn ("Invalid Index. Please choose a number between 1 and " ++ show options) >>
          loopAction results
-       ValidAction index action ->
-         let focus = show $ results !! (index - 1)
-         in putStrLn (show action ++ focus) >> E.exitSuccess
 
-askAction :: IO ()
-askAction = putStrLn "Please select a number and an action to perform. Actions can be one of (c) Copy to clipboard (b) Open in browser.\nSelect :h to go to the home screen or :q to quit"
+printActionAndExit :: T.Action -> [T.Entry] -> Int -> IO ()
+printActionAndExit action results index =
+  let focus = show $ results !! (index - 1)
+  in putStrLn (show action ++ focus) >> exit
 
+printMatchResults :: [T.Entry] -> IO ()
+printMatchResults entries = putStrLn $ L.intercalate "\n" ((\(index, entry) -> show index ++ ". " ++ show entry) <$> L.zip [(1::Int)..] entries)
 
-printMatchResults :: [T.Entry] -> String
-printMatchResults entries = L.intercalate "\n" ((\(index, entry) -> show index ++ ". " ++ show entry) <$> L.zip [(1::Int)..] entries)
+exit :: IO ()
+exit = E.exitSuccess
+
+exitWithConfigError :: String -> IO ()
+exitWithConfigError message = E.die $ "Failed to load config: " ++ message
+
