8c996ddfc90ccf99c8538cd60af3e99b656ee6bb
sanjiv sahayam <sanjsmailbox@gmail.com>
2018-01-28 23:27:05 +1100

    initial typeclass test

--- src/Classes.hs
+++ src/Classes.hs
@@ -1,18 +1,12 @@
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Classes(  ConsoleR(..)
                , SystemR (..)
                , ProcessR(..)
-               , ProgramR
-               , LogWriter) where
+               , ProgramR) where
 
 import qualified Process as P
 import qualified System.Exit as E
-import Text.Printf (printf)
-import Data.Functor.Identity
-import Control.Monad.Trans.Writer.Lazy
-import Control.Monad.Trans.Reader
 
 class Monad m => ConsoleR m where
   writeLine :: String -> m ()
@@ -20,11 +14,10 @@ class Monad m => ConsoleR m where
   readLine  :: m String
 
 class Monad m => SystemR m where
-  exit :: m a
+  exit :: m ()
 
 class Monad m => ProcessR m where
   launchShell :: String -> m (Either String P.LaunchResult)
-
 
 class (ConsoleR m, SystemR m, ProcessR m) => ProgramR m
 
@@ -33,38 +26,11 @@ instance ConsoleR IO where
   write     = putStr
   readLine  = getLine
 
-newtype Log = Log [String] deriving (Monoid, Show)
-
-type LogWriter = WriterT Log Identity
--- s -> m (a, w)
-type PrimedLogWriter = ReaderT String (WriterT Log Identity)
-
-instance ConsoleR LogWriter where
-  writeLine s = tell $ Log [printf "%s\n" s]
-  write     s = tell $ Log [s]
-  readLine  = writer ("some-string", Log ["read: some-string"])
 
 instance SystemR IO where
   exit = E.exitSuccess
 
 instance ProcessR IO where
   launchShell = P.launchShell
-
--- instance ProcessR LogWriter where
---   launchShell command = writer (Right (P.LaunchResult E.ExitSuccess "some-output" "some-err"), Log [command])
-
--- String -> m (Either String P.LaunchResult)
--- instance ProcessR PrimedLogWriter where
---   launchShell command =
---     do x <- reader (\other ->  Right (P.LaunchResult (E.ExitFailure 1)  "some-output1" "some-err1"))
---        _ <- return (tell (Log [command]))
---        (return . return . Right) x
-
-    -- do variant <- ask
-    --    let result = case variant of
-    --                   "success" -> writer (Right (P.LaunchResult (E.ExitFailure 1)  "some-output1" "some-err1"), Log [command])
-    --                   "failure" -> writer (Right (P.LaunchResult E.ExitSuccess "some-output2" "some-err2"), Log [command])
-    --                   x -> writer (Left x, Log [command])
-    --    result
 
 instance ProgramR IO
---
+++ test/RMSpec.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module RMSpec where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Data.Functor.Identity
+import Control.Monad.Trans.Writer.Lazy
+import Control.Monad.Trans.State.Lazy
+import Control.Monad.Trans.Class
+import Text.Printf (printf)
+import qualified System.Exit as E
+
+import Classes
+import qualified Process as P
+import qualified RM as R
+import qualified Types as T
+
+newtype Log = Log { unlog :: [String] } deriving (Monoid, Show)
+
+instance Monad m => ConsoleR (StateT String (WriterT Log m)) where
+  writeLine s = (lift . tell) $ Log [printf "%s\n" s]
+  write     s = (lift . tell) $ Log [s]
+  readLine  =  get >>= (\s -> lift (tell $ Log [s])) >> get
+
+instance Monad m => SystemR (StateT String (WriterT Log m)) where
+  exit = (lift . tell) $ Log ["exit"]
+
+instance Monad m => ProcessR (StateT String (WriterT Log m)) where
+  launchShell cmd =
+      lift $
+      writer (Right (P.LaunchResult E.ExitSuccess "this is sysout" "this is syserr"),
+              Log [printf "launchShell called with: %s" cmd])
+
+instance Monad m => ProgramR (StateT String (WriterT Log m))
+
+successfulHomeExitTest :: TestTree
+successfulHomeExitTest = testCase "exits from home screen" $
+                          let resultSWI = R.loopHome (T.AllEntries []) :: StateT String (WriterT Log Identity) ()
+                              log = (runIdentity . execWriterT . runStateT resultSWI) ":q"
+                          in (unlog log) @?= ["Enter a query or press :q to quit\n", ":q", "exit"]
+
+test_rm :: TestTree
+test_rm = testGroup "RM" [successfulHomeExitTest]
