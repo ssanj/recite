0b8bece7f4fa2f382b6a56f736dd2c20b88ac9b4
sanjiv sahayam <sanjsmailbox@gmail.com>
2018-01-30 23:36:14 +1100

    fix formatting

--- test/RMSpec.hs
+++ test/RMSpec.hs
@@ -49,14 +49,14 @@ runStack st cmd = (unlog . runIdentity . execWriterT) $ runStateT st cmd
 successfulHomeExitTest :: TestTree
 successfulHomeExitTest = testCase "exits from home screen" $
                           let resultSWI = R.loopHome (T.AllEntries []) :: StateT [String] (WriterT Log Identity) ()
-                              log = runStack resultSWI [":q"]
-                          in log @?= ["Enter a query or press :q to quit\n", ":q", "exit"]
+                              w         = runStack resultSWI [":q"]
+                          in w @?= ["Enter a query or press :q to quit\n", ":q", "exit"]
 
 invalidQueryTest :: TestTree
 invalidQueryTest = testCase "handles invalid query syntax" $
                     let resultSWI = R.loopHome (T.AllEntries []) :: StateT [String] (WriterT Log Identity) ()
-                        log = runStack resultSWI [">" ,":q"]
-                    in log @?=
+                        w         = runStack resultSWI [">" ,":q"]
+                    in w @?=
                         ["Enter a query or press :q to quit\n",
                          ">",
                          "your command was invalid. Format: command,[command]* > [?|^|*]\n",
@@ -65,14 +65,14 @@ invalidQueryTest = testCase "handles invalid query syntax" $
 
 validQueryTest :: TestTree
 validQueryTest = testCase "handle valid query" $
-                    let iphone  = T.entry "iPhone" "https://www.apple.com/iphone/" ["apple", "phone", "iphone"]
-                        ipad    = T.entry "iPad" "https://www.apple.com/ipad/" ["apple", "tablet", "ipad"]
-                        homepod = T.entry "HomePod" "https://www.apple.com/homepod/" ["apple", "speaker", "homepod"]
-                        pixel2  = T.entry "Pixel2" "https://store.google.com/product/pixel_2" ["google", "phone", "pixel2"]
-                        entries = T.AllEntries $ catMaybes [iphone, pixel2, ipad, homepod]
+                    let iphone    = T.entry "iPhone" "https://www.apple.com/iphone/" ["apple", "phone", "iphone"]
+                        ipad      = T.entry "iPad" "https://www.apple.com/ipad/" ["apple", "tablet", "ipad"]
+                        homepod   = T.entry "HomePod" "https://www.apple.com/homepod/" ["apple", "speaker", "homepod"]
+                        pixel2    = T.entry "Pixel2" "https://store.google.com/product/pixel_2" ["google", "phone", "pixel2"]
+                        entries   = T.AllEntries $ catMaybes [iphone, pixel2, ipad, homepod]
                         resultSWI = R.loopHome entries :: StateT [String] (WriterT Log Identity) ()
-                        log = runStack resultSWI ["apple" , "2 c", ":q"]
-                    in log @?=
+                        w         = runStack resultSWI ["apple" , "2 c", ":q"]
+                    in w @?=
                         ["Enter a query or press :q to quit\n",
                          "apple",
                          "searching for apple\n",
